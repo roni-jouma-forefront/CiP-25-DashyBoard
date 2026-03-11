1a32256ff73330fbf0785383f9efdba3c86fa1f5
Anna Hallberg <anna.hallberg@sveaskog.se>
2026-03-11 15:01:31 +0100

    changes from pr

--- frontend/src/components/base/GetWeather.tsx
+++ frontend/src/components/base/GetWeather.tsx
@@ -14,17 +14,22 @@ export type MetarData = {
   conditions: string | null;
 };
 
-export async function GetWeather( icao:string): Promise<MetarData> {
-    const res = await fetch(`http://localhost:5000/api/CheckWx/${icao.toUpperCase()}`, {
-        headers: { "Content-Type": "application/json"},
-    });
+export async function GetWeather(icao: string): Promise<MetarData> {
+  const res = await fetch(
+    `http://localhost:5000/api/CheckWx/${icao.toUpperCase()}`,
+    {
+      headers: { "Content-Type": "application/json" },
+    },
+  );
 
-      if (!res.ok) {
-    throw new Error(`Kunde inte hämta väder för ${icao}`);
+  if (!res.ok) {
+    throw new Error(`Cound't get weather info for ${icao}`);
   }
 
   const json = await res.json();
-  const item =Array.isArray(json) ? json[0] : json?.metarData ?? json?.data ?? json;
+  const item = Array.isArray(json)
+    ? json[0]
+    : (json?.metarData ?? json?.data ?? json);
 
   return item as MetarData;
 }
--- frontend/src/components/mirror/WeatherWidget.tsx
+++ frontend/src/components/mirror/WeatherWidget.tsx
@@ -6,36 +6,38 @@ import TableContainer from "@mui/material/TableContainer";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { useQuery } from "@tanstack/react-query";
-import { GetWeather, type MetarData} from '../base/GetWeather'
+import { GetWeather, type MetarData } from "../base/GetWeather";
 
 interface WeatherProps {
-  icao: string; 
+  icao: string;
 }
 
-function WeatherWidget( { icao }: WeatherProps) {
-  const { data: metarData, isLoading, error} = useQuery<MetarData> ({
+function WeatherWidget({ icao }: WeatherProps) {
+  const {
+    data: metarData,
+    isLoading,
+    error,
+  } = useQuery<MetarData>({
     queryKey: ["weather", icao],
     queryFn: () => GetWeather(icao),
     enabled: !!icao,
-  })
+  });
 
-  console.log("VÄDERDATA SOM HÄMTATS: ", metarData)
+  if (error) return <p>Error: {error.message}</p>;
+  if (isLoading) return <p>Loading weather info...</p>;
 
-  if (error) return <p>Fel: {(error).message}</p>;
-  if (isLoading) return <p>Laddar väder...</p>;
-
-function getWeatherIconClass(conditions: string | null) {
-  if (!conditions){
-    return "wi-na"
+  function getWeatherIconClass(conditions: string | null) {
+    if (!conditions) {
+      return "wi-na";
+    }
+    const c = conditions.toLowerCase();
+    if (c.includes("sun") || c.includes("clear")) return "wi-day-sunny";
+    if (c.includes("rain")) return "wi-rain";
+    if (c.includes("snow")) return "wi-snow";
+    if (c.includes("cloud")) return "wi-cloudy";
+    if (c.includes("storm")) return "wi-thunderstorm";
+    return "wi-na";
   }
-  const c = conditions.toLowerCase();
-  if (c.includes("sun") || c.includes("clear")) return "wi-day-sunny";
-  if (c.includes("rain")) return "wi-rain";
-  if (c.includes("snow")) return "wi-snow";
-  if (c.includes("cloud")) return "wi-cloudy";
-  if (c.includes("storm")) return "wi-thunderstorm";
-  return "wi-na";
-}
 
   return (
     <>
@@ -66,70 +68,73 @@ function getWeatherIconClass(conditions: string | null) {
           >
             Weather
           </Typography>
-          
-          
-                    {!metarData ? (
+
+          {!metarData ? (
             <Typography>Loading weather...</Typography>
           ) : (
             <>
-          
-           <TableContainer component={Paper} sx={{ maxWidth: 700 }}>
-            <Table>
-              <TableBody>
-                <TableRow sx={{ backgroundColor: "#f5f7fa" }}>
-                  <TableCell sx={{ fontWeight: "bold", width: "40%" }}>
-                    Airport:
-                  </TableCell>
-                  <TableCell>{metarData.station?.name ?? "-"}</TableCell>
-                </TableRow>
+              <TableContainer component={Paper} sx={{ maxWidth: 700 }}>
+                <Table>
+                  <TableBody>
+                    <TableRow sx={{ backgroundColor: "#f5f7fa" }}>
+                      <TableCell sx={{ fontWeight: "bold", width: "40%" }}>
+                        Airport:
+                      </TableCell>
+                      <TableCell>{metarData.station?.name ?? "-"}</TableCell>
+                    </TableRow>
 
-                <TableRow>
-                  <TableCell sx={{ fontWeight: "bold" }}>ICAO:</TableCell>
-                  <TableCell>{metarData.icao ?? "-"}</TableCell>
-                </TableRow>
+                    <TableRow>
+                      <TableCell sx={{ fontWeight: "bold" }}>ICAO:</TableCell>
+                      <TableCell>{metarData.icao ?? "-"}</TableCell>
+                    </TableRow>
 
-                <TableRow>
-                  <TableCell sx={{ fontWeight: "bold" }}>Wind (m/s):</TableCell>
-                  <TableCell>
-                   {metarData.windSpeedMps ?? "-"} m/s
-                  </TableCell>
-                </TableRow>
+                    <TableRow>
+                      <TableCell sx={{ fontWeight: "bold" }}>
+                        Wind (m/s):
+                      </TableCell>
+                      <TableCell>{metarData.windSpeedMps ?? "-"} m/s</TableCell>
+                    </TableRow>
 
-                <TableRow>
-                  <TableCell sx={{ fontWeight: "bold" }}>Temperature:</TableCell>
-                  <TableCell>
-                    {metarData.temperature?.celsius ?? "-"}°C /{" "}
-                    {metarData.temperature?.fahrenheit ?? "-"}°F
-                  </TableCell>
-                </TableRow>
+                    <TableRow>
+                      <TableCell sx={{ fontWeight: "bold" }}>
+                        Temperature:
+                      </TableCell>
+                      <TableCell>
+                        {metarData.temperature?.celsius ?? "-"}°C /{" "}
+                        {metarData.temperature?.fahrenheit ?? "-"}°F
+                      </TableCell>
+                    </TableRow>
 
-                <TableRow sx={{ backgroundColor: "#f5f7fa" }}>
-                  <TableCell sx={{ fontWeight: "bold" }}>Humidity:</TableCell>
-                  <TableCell>
-                    {metarData.humidity ?? "-"}%
-                  </TableCell>
-                </TableRow>
+                    <TableRow sx={{ backgroundColor: "#f5f7fa" }}>
+                      <TableCell sx={{ fontWeight: "bold" }}>
+                        Humidity:
+                      </TableCell>
+                      <TableCell>{metarData.humidity ?? "-"}%</TableCell>
+                    </TableRow>
 
-                <TableRow>
-                  <TableCell sx={{ fontWeight: "bold" }}>Conditions:</TableCell>
-                  <TableCell
-                    sx={{ display: "flex", alignItems: "center", gap: 1 }}
-                  ><i className={ `wi ${getWeatherIconClass(metarData.conditions)}`}
-              ></i>
-              <i className={"wi-cloudy"}></i>
-                    {metarData.conditions ?? "-"}
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer> 
-           </>
+                    <TableRow>
+                      <TableCell sx={{ fontWeight: "bold" }}>
+                        Conditions:
+                      </TableCell>
+                      <TableCell
+                        sx={{ display: "flex", alignItems: "center", gap: 1 }}
+                      >
+                        <i
+                          className={`wi ${getWeatherIconClass(metarData.conditions)}`}
+                        ></i>
+                        <i className={"wi-cloudy"}></i>
+                        {metarData.conditions ?? "-"}
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </>
           )}
-        </Box> 
+        </Box>
       </Box>
     </>
   );
 }
 
 export default WeatherWidget;
-
