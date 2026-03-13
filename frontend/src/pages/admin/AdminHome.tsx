d30c54c876041881c6f46521a0dba25bd7ad97a3
Emmi Quirin <emmi.quirin@forefront.se>
2026-03-13 18:11:18 +0100

    feat(admin): pass initialMessages from AdminHome to MessageAccordion

--- frontend/src/pages/admin/AdminHome.tsx
+++ frontend/src/pages/admin/AdminHome.tsx
@@ -1,16 +1,19 @@
 import { DashboardForm } from "../../components/admin/forms/DashboardForm";
 import Watch from "../../components/base/watch";
 import { Stack, Typography } from "@mui/material";
-import { MessageAccordion } from "../../components/admin/MessageAccordion";
+import { MessageAccordion } from "../../components/admin/messages/MessageAccordion";
+import messageService from "../../components/admin/messages/messageService";
 
 export default function AdminHome() {
+  const initialMessages = messageService();
+
   return (
     <>
       <Typography variant="h2">DashyBoard</Typography>
       <Watch location="Stockholm" timeZone="UTC"></Watch>
       <Stack direction="row" spacing={2} alignItems="flex-start">
         <DashboardForm />
-        <MessageAccordion />
+        <MessageAccordion initialMessages={initialMessages}/>
       </Stack>
     </>
   );
