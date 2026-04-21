b63fa92edddaed7b48f3753950cc7780f5e8ec43
Emmi Quirin <emmi.quirin@forefront.se>
2026-04-21 14:01:58 +0200

    Add getMessages service and useMessages hook with react-query

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,5 +1,6 @@
 export { useWeather } from "./useWeather";
-export { useFlightInfo } from "./useFlightInfo"
+export { useFlightInfo } from "./useFlightInfo";
+export { useMessages } from "./useMessages";
 
-export { useDepartureFlights } from "./useDepartureFlights"
-export { useArrivalFlights } from "./useArrivalFlights"
+export { useDepartureFlights } from "./useDepartureFlights";
+export { useArrivalFlights } from "./useArrivalFlights";
