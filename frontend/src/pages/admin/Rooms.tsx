8bfd804871a38694b56c21c928b4c0c572dec2bd
Emmi Quirin <emmi.quirin@forefront.se>
2026-04-27 14:37:40 +0200

    Query for rooms with bookings on rooms page

--- frontend/src/pages/admin/Rooms.tsx
+++ frontend/src/pages/admin/Rooms.tsx
@@ -1,12 +1,25 @@
 import { Alert, Typography } from "@mui/material";
 import { RoomsAccordion } from "../../components/admin/RoomsAccordion";
+import { getRoomsWithBookings } from "../../services/api/getRoomsWithBooking";
+import { useQuery } from "@tanstack/react-query";
 
 export default function RoomAdminPage() {
+  const {
+    data: rooms = [],
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["rooms"],
+    queryFn: getRoomsWithBookings,
+  });
+
   return (
     <>
       <Typography variant="h2">Rooms</Typography>
-      <Alert severity="info">Search and filtering features are currently in development</Alert>
-      <RoomsAccordion />
+      <Alert severity="info">
+        Search and filtering features are currently in development
+      </Alert>
+      <RoomsAccordion rooms={rooms} error={error} isLoading={isLoading} />
     </>
   );
 }
