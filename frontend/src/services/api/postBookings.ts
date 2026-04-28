be6ab23e09c42c015b4b0a4fe9bef2a825014ec1
Emmi Quirin <emmi.quirin@forefront.se>
2026-04-28 15:14:00 +0200

    Fixed spelling error

--- frontend/src/services/api/postBookings.ts
+++ frontend/src/services/api/postBookings.ts
@@ -8,7 +8,7 @@ export async function postBookings(FormData: FormData) {
   });
 
   if (!res.ok) {
-    throw new Error(`Cound't post bookings file}`);
+    throw new Error(`Couldn't post bookings file`);
   } else {
     return "Posted";
   }
