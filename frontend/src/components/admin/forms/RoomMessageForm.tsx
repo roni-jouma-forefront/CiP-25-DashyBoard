302dbfd8e2407b9e338feb1f19a73ff064b11df5
Emmi Quirin <emmi.quirin@forefront.se>
2026-05-12 14:59:55 +0200

    Uppdate room message form to new combined fields

--- frontend/src/components/admin/forms/RoomMessageForm.tsx
+++ frontend/src/components/admin/forms/RoomMessageForm.tsx
@@ -40,17 +40,12 @@ export const RoomMessageForm = ({ onSubmit, bookingId }: RoomFormProps) => {
     }));
   };
 
-  const onPostDateChange = (value: Dayjs | null) => {
+  const onPostDateTimeChange = (value: Dayjs | null) => {
     setFormData((prev) => ({ ...prev, postDate: value }));
   };
-  const onPostTimeChange = (value: Dayjs | null) => {
-    setFormData((prev) => ({ ...prev, postTime: value }));
-  };
-  const onExpiresDateChange = (value: Dayjs | null) => {
+
+  const onExpiresDateTimeChange = (value: Dayjs | null) => {
     setFormData((prev) => ({ ...prev, expiresDate: value }));
-  };
-  const onExpiresTimeChange = (value: Dayjs | null) => {
-    setFormData((prev) => ({ ...prev, expiresTime: value }));
   };
 
   const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
@@ -98,10 +93,8 @@ export const RoomMessageForm = ({ onSubmit, bookingId }: RoomFormProps) => {
       <Stack spacing={3}>
         <MessageBaseForm
           handleChange={handleChange}
-          onPostTimeChange={onPostTimeChange}
-          onPostDateChange={onPostDateChange}
-          onExpiresTimeChange={onExpiresTimeChange}
-          onExpiresDateChange={onExpiresDateChange}
+          onPostDateTimeChange={onPostDateTimeChange}
+          onExpiresDateTimeChange={onExpiresDateTimeChange}
           title={formData.title}
           content={formData.content}
         />
