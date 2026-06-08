8bb201b0ad2e30fa1be084759a260bf4cf1a5586
Emmi Quirin <emmi.quirin@forefront.se>
2026-06-08 14:10:20 +0200

    Simplify Settings page layout by removing unused staff form

--- frontend/src/pages/admin/Settings.tsx
+++ frontend/src/pages/admin/Settings.tsx
@@ -1,6 +1,5 @@
 import { Stack, Typography } from "@mui/material";
 import { SettingsForm } from "../../components/admin/forms/SettingsForm";
-import { AddStaffForm } from "../../components/admin/forms/AddStaffForm";
 
 export default function SettingsPAge() {
   return (
@@ -8,7 +7,6 @@ export default function SettingsPAge() {
       <Typography variant="h2">Settings</Typography>
       <Stack direction="row" spacing={2} alignItems="flex-start">
         <SettingsForm />
-        <AddStaffForm />
       </Stack>
     </>
   );
